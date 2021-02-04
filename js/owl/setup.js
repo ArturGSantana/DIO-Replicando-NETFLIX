45b66451e57d38f0fc4a341d07db19c54cb8eff0
ArturGSantana <arturg.santana@gmail.com>
2021-02-03 23:47:15 -0300

    primeiro commit do projeto

---
+++ js/owl/setup.js
@@ -0,0 +1,57 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:5,
+    nav:true,
+    navText:["Anterior", "Próximo"],
+    autoplay: true,
+    autoplayTimeout: 2000,
+
+
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+/*
+$('.owl-carousel-elenco').owlCarousel({
+    loop:true,
+    margin:5,
+    nav:true,
+    navText:["Anterior", "Próximo"],
+    autoplay: true,
+    autoplayTimeout: 2000,
+
+
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+*/
+function direciona() {
+    window.location.href="https://www.youtube.com/watch?v=XrVlzrRECY4"
+    
+}
+
+function direcionaInfo() {
+    window.location.href="bioBrBd.html"
+}
+
+function direciona2() {
+    window.location.href="https://www.youtube.com/watch?v=XrVlzrRECY4"
+    
+}
